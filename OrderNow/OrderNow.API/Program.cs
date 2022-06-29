d07eaeac28456633181d3b4f0d9973b9d4f5a2ed
Emiliano Policardo <emilianopolicardo@gmail.com>
2022-06-29 10:34:59 -0300

    Versionado completo

--- OrderNow/OrderNow.API/Program.cs
+++ OrderNow/OrderNow.API/Program.cs
@@ -124,7 +124,11 @@ if (app.Environment.IsDevelopment())
 
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.SwaggerEndpoint("/swagger/v2/swagger.json", "My API V2");
+    });
 }
 
 app.UseHangfireDashboard("/jobs");
